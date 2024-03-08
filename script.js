0f7fa0f18140b1ad983c628f8047f5a46497996f
DevJpFarias <joaopfc2764@gmail.com>
2024-03-08 00:13:07 -0300

    create project

---
+++ script.js
@@ -0,0 +1,25 @@
+function calculateCompoundInterest() {
+  const principal = parseFloat(document.getElementById("principal").value);
+  const rate = parseFloat(document.getElementById("rate").value);
+  const months = parseFloat(document.getElementById("months").value);
+  const monthly = parseFloat(document.getElementById("monthly").value);
+
+  let amount = principal;
+
+  for (let i = 1; i <= months; i++) {
+      amount = amount * (1 + rate) + monthly;
+  }
+
+  const resultDiv = document.getElementById("result");
+
+  resultDiv
+      .innerHTML =
+      "Montante final: " +
+      amount.toLocaleString(
+          'pt-br',
+          {
+              style: 'currency',
+              currency: 'BRL'
+          }
+      );
+}
